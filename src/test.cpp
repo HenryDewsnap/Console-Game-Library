58aea6fec6281d1edfac6b2d84a16265b5f65aa2
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-02 00:04:19 +0000

    Create test.cpp

---
+++ src/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#pragma
+#include "output/graphicManager.hpp"
+
+int main() {
+    windowObject win(25,25,1,{0,0});
+    win.updateFgColour(4);
+
+    for (int i=0; i<6; i++) {
+        win.loadStandardShapeToBuffer("square", {i*4,i*4}, 4);
+    }
+
+    win.plotBuffer();
+    win.clearBuffer();
+
+    win.updateFgColour(5);
+    win.drawLine({{0,4},{20,24}});
+    win.drawLine({{4,0},{24,20}});
+
+    for (int i=0; i<6; i++) {
+        win.loadStandardShapeToBuffer("square", {20-(i*4),(i*4)}, 4);
+    }
+    win.updateFgColour(6);
+    win.plotBuffer();
+    win.clearBuffer();
+
+    win.updateFgColour(8);
+    win.drawLine({{5,3},{17,21}});
+
+    std::cin.get();
+}
